ab55dbcf2a2d66f07efeb5ec633f558050bd8667
Brian Howell1 <Brian.Howell@gfs.com>
2019-02-21 17:44:34 -0500

    initial checkin of my changes to my fork of robbyrussell's work

--- plugins/terraform/terraform.plugin.zsh
+++ plugins/terraform/terraform.plugin.zsh
@@ -4,6 +4,6 @@ function tf_prompt_info() {
     # check if in terraform dir
     if [ -d .terraform ]; then
       workspace=$(terraform workspace show 2> /dev/null) || return
-      echo "[${workspace}]"
+      echo -n "${workspace}"
     fi
 }
